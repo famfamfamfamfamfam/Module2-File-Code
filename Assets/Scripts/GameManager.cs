0d0f60faa37fa325461380868762fe3b66cddcf2
BinhTran <fam.out16@gmail.com>
2024-11-10 18:00:39 +0700

    Final

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,7 +12,8 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     private void Awake()
     {
-        instance = null;
+        if (instance != null)
+            Destroy(instance.gameObject);
         instance = this;
     }
 
